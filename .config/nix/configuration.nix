0bb6f96b11b45462ece165cc2349f82cd07da32d
acnimda <acnimda@gmail.com>
2023-10-18 13:20:45 +0200

    initial commit

--- .config/nix/configuration.nix
+++ .config/nix/configuration.nix
@@ -12,9 +12,15 @@
       ./hardware-configuration.nix
     ];
 
+  # Bootloader VMware
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  #boot.loader.systemd-boot.enable = true;
+  #boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "gordito"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
