d944f59e441b4f5eba9f82233fd85f398a67b42f
acnimda <acnimda@gmail.com>
2023-10-19 09:52:33 +0200

    add home-manger script

---
+++ scripts/install_home-manager.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
+nix-channel --update && nix-shell '<home-manager>' -A install
+exit 0
